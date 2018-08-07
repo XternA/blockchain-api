78310012640a8b24b0f88227959c49af1216719a
Huy <hdich@tpinformation.com>
2018-08-07 16:05:13 +0100

    Changed request param to path variable instead

--- src/main/java/info/blockchain/controller/RequestController.java
+++ src/main/java/info/blockchain/controller/RequestController.java
@@ -4,7 +4,7 @@ import info.blockchain.handler.IHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +20,9 @@ public final class RequestController {
         this.handler = handler;
     }
 
-    @GetMapping(value = "/address")
+    @GetMapping(value = "/address/{wallet}")
     @ResponseBody
-    public final ResponseEntity<?> getUnspentTransaction(@RequestParam(value = "wallet") String walletAddress) {
+    public final ResponseEntity<?> getUnspentTransaction(@PathVariable(value = "wallet") String walletAddress) {
         return new ResponseEntity<>(handler.getUnspentTransactions(walletAddress), HttpStatus.OK);
     }
 }
