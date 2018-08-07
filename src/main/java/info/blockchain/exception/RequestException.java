5aa0bcfd7596c29beefa75ebbd6c61c9afc7ea2f
XternA <xternia@gmail.com>
2018-08-07 21:43:28 +0100

    Changed RequestException constructor overriding.

--- src/main/java/info/blockchain/exception/RequestException.java
+++ src/main/java/info/blockchain/exception/RequestException.java
@@ -2,7 +2,7 @@ package info.blockchain.exception;
 
 public class RequestException extends Exception {
 
-    public RequestException(String message) {
-        super(message);
+    public RequestException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
